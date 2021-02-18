fe0b74973915bbb863c958dc4671279de6e90085
Maharek <amaharek@a483e70b8e1d.ant.amazon.com>
2021-02-18 09:39:01 +0000

    another fix issue 85

--- src/sagemaker_pytorch_serving_container/torchserve.py
+++ src/sagemaker_pytorch_serving_container/torchserve.py
@@ -111,6 +111,12 @@ def _adapt_to_ts_format(handler_service):
     if not os.path.exists(DEFAULT_TS_MODEL_DIRECTORY):
         os.makedirs(DEFAULT_TS_MODEL_DIRECTORY)
 
+    extra_files = []
+    extra_files.append(os.path.join(environment.model_dir, DEFAULT_TS_CODE_DIR, environment.Environment().module_name + ".py"))
+    extra_files+= [os.path.join(environment.model_dir, file) for file
+                                    in os.listdir(environment.model_dir)
+                                    if os.path.isfile(os.path.join(environment.model_dir, file)) and file != DEFAULT_TS_MODEL_SERIALIZED_FILE ]
+
     model_archiver_cmd = [
         "torch-model-archiver",
         "--model-name",
